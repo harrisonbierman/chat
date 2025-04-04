84add52749a3c48d3411d593df1a534f89608a5e
harrison <harryscout@gmail.com>
2025-04-04 16:51:31 -0700

    made a eoll server that will echo back anything the clients send to it

--- client.c
+++ client.c
@@ -8,31 +8,29 @@
 #define BUFFER_SIZE 1024
 
 int main() {
-	int sock = 0;
-	struct sockaddr_in serv_addr;
-	char *message = "Hello, Server!";
-	char buffer[BUFFER_SIZE] = {0};
+
+	// define socket address
+	struct sockaddr_in server_address = {
+		.sin_family = AF_INET,
+		.sin_port = htons(PORT),
+	};
 
 	// Create a socket (IPv4, TCP
-	sock = socket(AF_INET, SOCK_STREAM, 0);
-	if (sock == -1) {
+	int client_fd = socket(AF_INET, SOCK_STREAM, 0);
+	if (client_fd == -1) {
 		perror("socket creating failed\n");
 		exit(EXIT_FAILURE);
 	}
-	
-	// define the socket address 
-	serv_addr.sin_family = AF_INET;
-	serv_addr.sin_port = htons(PORT);
 
 	// Convert IPv4 address from text to binary form
-	// store it into the serv_addr.sin_addr
-	if (inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr) <= 0) {
+	// store it into the server_address.sin_addr
+	if (inet_pton(AF_INET, "127.0.0.1", &server_address.sin_addr) <= 0) {
 		perror("Invalid address/ Address not supported\n");
 		exit(EXIT_FAILURE);
 	}
 
 	// Connect to server
-	if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
+	if (connect(client_fd, (struct sockaddr *)&server_address, sizeof(server_address)) < 0) {
 		perror("Connection Failed\n");
 		exit(EXIT_FAILURE);
 	}
@@ -40,22 +38,22 @@ int main() {
 	
 	
 	while (1) {
-		char user_input[120];
+		char buf[1024];
 		printf("write something to the server: ");
-		fgets(user_input, sizeof(user_input), stdin);
+		fgets(buf, sizeof(buf), stdin);
 		printf("\033[A");
 		printf("\r\033[2K");
 
 		// Send the Message
-		send(sock, user_input, strlen(user_input), 0);
-		printf("client: %s", user_input);
+		send(client_fd, buf, strlen(buf), 0);
+		printf("client: %s", buf);
 
-		int valread = read(sock, buffer, BUFFER_SIZE);
-		printf("server: %s", buffer);
+		int valread = read(client_fd, buf, BUFFER_SIZE);
+		printf("server: %s", buf);
 		
 	}
 	
 	//Close the socket
-	close(sock);
+	close(client_fd);
 	return 0;
 }
--- server.c
+++ server.c
@@ -2,18 +2,38 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <fcntl.h>
+#include <sys/epoll.h>
+#include <errno.h>
 #include <arpa/inet.h>
 
 #define PORT 65432
 #define BUFFER_SIZE 1024
+#define MAX_EVENTS 10
+
+void make_socket_non_blocking(int fd) {
+	int flags = fcntl(fd, F_GETFL, 0);
+	// the reason for this is to add O_NONBLOCK to the 
+	// existing flags and not to override them
+	// since the options use bit position using the 
+	// or operator combinds the bits together 
+	// ex: 0010 | 1000 = 1010
+	fcntl(fd, F_SETFL, flags | O_NONBLOCK);
+}
 
 int main() {
 	// server_fd = server file descriptor, refers to socket created
 	int server_fd, client_socket;		
+
 	// a struct from <netinet/in.h> from <arpa/inet.h>
-	struct sockaddr_in address; 
+	struct sockaddr_in address = {
+		// Bind the socket to localhost and PORT
+		.sin_family = AF_INET,
+		.sin_addr.s_addr = INADDR_ANY,
+		.sin_port = htons(PORT), // host to network byte order short
+	};
+
 	int opt = 1;
-	int addrlen = sizeof(address);
 	char buffer[BUFFER_SIZE] = {0};
 
 	// Create a Socket (IPv4, TCP)
@@ -21,11 +41,12 @@ int main() {
 	// third agrument '0' is an unspecified default protocol
 	// returns a non-negative integer if successful
 	server_fd = socket(AF_INET, SOCK_STREAM, 0);
-	printf("this is the servers listening socket: %d\n", server_fd);
 	if (server_fd == -1) {
 		perror("Socket creation failed");
 		exit(EXIT_FAILURE);
 	}
+	// makes functions like accept() nonblocking obvi
+	make_socket_non_blocking(server_fd);
 
 	// set socket option to reuse address and port
 	if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt))) {
@@ -33,17 +54,12 @@ int main() {
 		exit(EXIT_FAILURE);
 	}
 
-	// Bind the socket to localhost and PORT
-	address.sin_family = AF_INET;
-	address.sin_addr.s_addr = INADDR_ANY;
-	address.sin_port = htons(PORT); // host to network byte order short
-	
 	// bind the address to the socket
 	// "I want this socket reachable at this address"
 	// Cast the less generic sockaddr_in struct to the more 
 	// generic sockaddr struct for the bind function
 	if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0) {
-		perror("Bind failed");
+		perror("Server bind failed");
 		exit(EXIT_FAILURE);
 	}
 
@@ -52,50 +68,62 @@ int main() {
 		perror("Listen");
 		exit(EXIT_FAILURE);
 	}
+
 	printf("Server is listening on port %d\n", PORT);
 
+	int epoll_fd = epoll_create1(0);
 
+	// set up event that watches for incoming traffic
+	// with EPOLLIN = check if there is read data
+	struct epoll_event server_event = {
+		.events = EPOLLIN,
+		.data.fd = server_fd,
+	};
+
+	// adding the server_fd to the epoll_fd instance
+	epoll_ctl(epoll_fd, EPOLL_CTL_ADD, server_fd, &server_event);
+
+	struct epoll_event events[MAX_EVENTS];
+
+	// event loop
 	while (1) {
-		// Accept on incoming connection
-		// the socket description, the address of the socket, and the length of the socket
-		client_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen);
-		if (client_socket < 0) {
-			perror("Accept Failed");
-			continue;
-		}
-
-		int pid = fork();
-		if (pid == 0) {
-			// child process
-			close(server_fd); // child doesn't need listening socket
-
-			printf("New Client Connected FD: %d\n", client_socket);
-
-			clear(0);
-			while (1) {	
-				// Read data from the client
-				memset(buffer, 0, BUFFER_SIZE);// clears to buffer for next read
-				int valread = read(client_socket, buffer, BUFFER_SIZE);
-				if (valread == 0) {
-					printf("client disconnected");
-					break;
-				}
-				printf("client: %s", buffer);
+		// returns how many events are on the ready list
+		// -1 blocks indefinitely until at least one event is ready
+		printf("waiting for next event...\n");
+		int n = epoll_wait(epoll_fd, events, MAX_EVENTS, -1);
+		printf("event deployed\n");
+		for (int i = 0; i < n; i++) {
+			// if server is on the ready list
+			if (events[i].data.fd == server_fd) {
+				// accept new connection
+				// null for now because we dont care about the
+				// client address information
+				int client_fd = accept(server_fd, NULL, NULL);
+				make_socket_non_blocking(client_fd);
 				
-				//send messgage to client
-				char user_input[120];
-				printf("Send a message to the client: ");
-				fgets(user_input, sizeof(user_input), stdin);
-				printf("\033[A");
-				printf("\r\033[2K");
+				// EPOLLIN = check if there is read data
+				struct epoll_event client_event = {
+					.events = EPOLLIN,
+					.data.fd = client_fd,
+				};
 
-				send(client_socket, user_input, sizeof(user_input), 0);
-				printf("server: %s", user_input);
+				epoll_ctl(epoll_fd, EPOLL_CTL_ADD, client_fd, &client_event);
+				printf("Accepted new client: %d\n", client_fd);
+			} else {
+				// if it is a client event
+				char buf[1024];
+				int valread = read(events[i].data.fd, buf, sizeof(buf));
+				if (valread <= 0) {
+					// client disconnected or error
+					close(events[i].data.fd);
+					printf("Closed client: %d\n", events[i].data.fd);
+				} else {
+					// Echo back
+					write(events[i].data.fd, buf, valread);
+				}
 			}
-			//close the connection and socket
-			close(client_socket);
-			close(server_fd);
-			return 0;
 		}
 	}
+
+	return(0);
 }
