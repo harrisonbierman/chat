d87bf28d08bbf9571c274635280501e116d2c3a2
harrison <harryscout@gmail.com>
2025-04-15 23:06:53 -0700

    removed .clangd.yml, wasn't doing anything

--- main.c
+++ main.c
@@ -1,21 +1,7 @@
-#include <netinet/in.h>
 #include <raylib.h>
-#include <arpa/inet.h>
-#include <sys/socket.h>
-
-#define PORT 65432
 
 int main() { 
 
-	struct sockaddr_in sock_addr = {
-		.sin_family = AF_INET, 
-		.sin_port = htons(PORT)
-	};
-
-	int client_fd = socket(AF_INET, SOCK_STREAM, 0);
-	
-	
-	
 	// init app
 	InitWindow(600, 500, "amazing window");
 	// run app
