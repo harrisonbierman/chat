96777524ea5828baed8779d8123cc44ed1d3bb70
harrison-pc <harryscout@gmail.com>
2025-04-26 20:32:54 -0700

    Add test feature: This was a test to see how to squash merge a branch into main, I commited 4 times to the test branch but there should only be one commit now for this feature

---
+++ test.c
@@ -0,0 +1,4 @@
+// this is a test file to test fetching branches
+// this is some other text that I made on the desktop
+// this is some more text from the laptop
+// this is the last thing I need to complete my test feature
